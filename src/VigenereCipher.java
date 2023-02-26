824d58ff5b9f8494f333e624c51d68638ba113b3
carolynh2025 <carolynh2025@student.cis.edu.hk>
2023-02-26 16:38:40 +0800

    d

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -1,59 +1,32 @@
 public class VigenereCipher {
-
-    //properties
-
     private String key;
-
     private String alphabet;
 
-
-
-    //constructors
-
     public VigenereCipher(String userKey){
-
         key = userKey.toLowerCase();
 
         alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
     }
 
-
-
-    //methods
-
     public String getKey() {
-
         return key;
-
     }
 
     public void setKey(String key) {
-
         this.key = key;
-
     }
 
     public String getAlphabet() {
-
         return alphabet;
-
     }
 
     public String encode(String userEncode){
-
         String encryptedWord = "";
 
-
-
-        for (int i = 0, j = 0; i < userEncode.length(); i++)
-
-        {
-
+        for (int i = 0, j = 0; i < userEncode.length(); i++) {
             char ofWord = userEncode.charAt(i);
 
             if (alphabet.indexOf(ofWord)==-1){
-
                 encryptedWord += ofWord;
             }
 
@@ -65,34 +38,25 @@ public class VigenereCipher {
 
         }
         return encryptedWord;
-
     }
 
-
-
     public String decode(String userDecode){
-
         String decryptedWord = "";
 
         for (int i = 0, j = 0; i < userDecode.length(); i++) {
-
             char ofWord = userDecode.charAt(i);
 
             if (alphabet.indexOf(ofWord)==-1){
-
                 decryptedWord+= ofWord;
             }
 
             else if (alphabet.indexOf(ofWord)>-1)
-
                 decryptedWord += (char) ((ofWord - key.charAt(j) + 26) % 26 + 'a');
+
             j = ++j % key.length();
+
             System.out.println(decryptedWord);
-
-
         }
         return decryptedWord;
-
     }
-
 }
